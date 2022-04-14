910194683287ba002cebd199c11b41f572418c95
raninder dhillon <raninderdhillon@gmail.com>
2022-04-14 06:28:38 -0400

    cleanup code

--- hatchways/student_profile/src/components/Fetch.jsx
+++ hatchways/student_profile/src/components/Fetch.jsx
@@ -1,83 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Student from './Student';
 import Search from './Search';
-// import { useHistory } from 'react-router-dom';
 
 export default function Fetch() {
 	const [data, setData] = useState([]);
-  // const [filteredData, setFilteredData] = useState([]);
-  // const history = useHistory();
+ 
   const url = "https://api.hatchways.io/assessment/students";
   useEffect(() => {
     axios.get(url)
     .then((response) => {
       console.log("data1",response.data.students);
       setData(response.data.students);
-      // setFilteredData(response.data);
     })
     .catch(error => {
       console.log('Error: ' + error);
       })
   }, []);
 	console.log("data2",data);
-
-
-  // function handleSearch(event){
-  //   let value = event.target.value;
-  //   let result = [];
-
-    // for(let item of data){
-    //   if(item.firstName.toLowerCase().includes(value)){
-    //   result.push(item);
-    //   }
-    // }
-    
-    // history.push('/Search', { result});
-    // let value = event.target.value;
-    // for(let item of data){
-    //   if(value === '')
-    //   setData(item);
-    //   else if(item.firstName.toLowerCase().includes(value))
-    //     setData(item);
-    //     else
-    //     setData('');
-    // }
-  //  filteredData = data.filter((el) => {
-	
-  //     if (value === '') {
-  //         return el;
-  //     }
-  
-  //     else {
-  //       return el.firstName.toLowerCase().includes(value);
-      
-  //     }
-  // })
-
-
-	
 	return(
 		<>
-     
-      <section className='products'>
-        <span id='search'>
+      <section>
           <Search data={data}/>
-        </span>
-        {/* <h1> Fragrances</h1> */}
-        {/* <div className='new-arrival-products'>
-          <ProductsList products={products} /> 
-        </div> */}
       </section>
-  
-		{/* <input type="text"
-          onChange={(event) => handleSearch(event)}
-          label="Search"
-          placeholder="search"
-        /> */}
-		{/* {data.map((item) => (
-        <Student key={item.id} {...item} />
-      ))} */}
 		</>
 	)
 }
--- hatchways/student_profile/src/components/Student.jsx
+++ hatchways/student_profile/src/components/Student.jsx
@@ -1,8 +1,5 @@
 import React, { useState,useEffect } from "react";
 import axios from "axios";
-// import { useAlert } from 'react-alert';
-// import "../styles/DispGoods.css";
-// import { useHistory } from 'react-router-dom';
 
 export default function Student(props) {
   const { id,pic,firstName, lastName, email,company, skill, grades} = props;
@@ -11,11 +8,8 @@ export default function Student(props) {
 	console.log("grades-i",grades[i]);
 	sum+=Number(grades[i]);
 	}
-	console.log("sum",sum);
+
 	const average = sum / grades.length;
-	console.log("avg",average);
-	// console.log("grades",grades[0]);
-	// const average = grades.reduce((a,b) => a + b, 0) / grades.length;
 	
   return (
     
@@ -28,8 +22,7 @@ export default function Student(props) {
         </div>
 			</div>
         <div className="details">
-            {/* <section class="new-tweet"></section> */}
-					{/* <img src = {pic} /> <br/> */}
+      
 					<h3 className="name">{firstName} {lastName}</h3>
 					<div className= "info">
 					Email: {email} <br/>
