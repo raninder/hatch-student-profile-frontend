4caded81e93cbd2a26130807140accb0f3f102dd
raninder dhillon <raninderdhillon@gmail.com>
2022-04-13 20:39:19 -0400

    styling added-part2

--- hatchways/student_profile/src/App.js
+++ hatchways/student_profile/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Fetch from './components/Fetch';
 import './App.css';
 
--- hatchways/student_profile/src/components/Student.jsx
+++ hatchways/student_profile/src/components/Student.jsx
@@ -19,16 +19,28 @@ export default function Student(props) {
 	
   return (
     
-  <div >
-					<img src = {pic} /> <br/>
-					<h3>{firstName} {lastName}</h3><br/>
+		 <section className="container">
+			 <div className="left">
+        <div className="image-cropper">
+
+            <img src={pic} className="profile-pic" />
+
+        </div>
+			</div>
+        <div className="details">
+            {/* <section class="new-tweet"></section> */}
+					{/* <img src = {pic} /> <br/> */}
+					<h3 className="name">{firstName} {lastName}</h3>
+					<div className= "info">
 					Email: {email} <br/>
 					Company: {company} <br/>
 					Skill: {skill} <br/>
 					Average: {average}%
+					</div>
+				</div>	
 					{/* <button className="btn btn-primary"onClick={() => add_to_request(id) }>Request</button>  */}
-				
-	</div>
+			
+	</section>
 
   );
 }
