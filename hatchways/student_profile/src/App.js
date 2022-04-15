d2daa797902071b08161277d9139a926bde03acf
raninder dhillon <raninderdhillon@gmail.com>
2022-04-14 22:22:06 -0400

    responsive style added

--- hatchways/student_profile/src/App.js
+++ hatchways/student_profile/src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import Fetch from './components/Fetch';
-import Student from './components/Student';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Fetch />
-      {/* <Student /> */}
     </div>
   );
 }
